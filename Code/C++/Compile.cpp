32b70d2fa14b91fee363b040882c1965a91772c4
amundmr <amund.raniseth@gmail.com>
2019-09-12 21:29:46 +0200

    Compiler program compiles AND runs whatever you throw at it.

--- Code/C++/Compile.cpp
+++ Code/C++/Compile.cpp
@@ -1,16 +1,62 @@
-#include <iostream> //In out CLI
-#include <fstream>  //Read write files
-#include <cstdlib>  //String to float conversion
-#include <string>   //Simple stringing
+/*
+This program compiles and runs the requested code.
+*/
 
-using namespace std
+#include <iostream> // In out CLI
+#include <fstream>  // Read write files
+#include <string>   // Simple stringing
+
+using namespace std;
+
+void simpleCompile(string inFile) {
+  // Removing .cpp from infile and concatenating input strings to commands
+  string outFile = inFile.substr(0, inFile.size()-4);
+  string compileCommand = "c++ -o " + outFile + " " + inFile;
+  string runCommand = "./" + outFile;
+
+  // Make system compile and run file
+  system(compileCommand.c_str());
+  system(runCommand.c_str());
+}
+
+
+void compile(string outFile, string inFile) {
+  // Concatenating input strings to commands
+  string compileCommand = "c++ -o " + outFile + " " + inFile;
+  string runCommand = "./" + outFile;
+
+  // Make system compile and run file
+  system(compileCommand.c_str());
+  system(runCommand.c_str());
+}
 
 int main(int argc, char **argv) {
-  if((argc < 2) || (argc > 2)) {
-    cout << "This program takes filename to compile and run as input variable." << endl;
-    throw "Write an input variable!";
+
+  for (int i = 1; i < argc; ++i) {            // Checkin every input argument for help command -h or --help
+    string arg = argv[i];
+    if ((arg == "-h") || (arg == "--help")){  // Displaying help information
+      cout << "This program can take either one or two input variables."<< endl;
+      cout << "Outfile is the compiled file you want to save, and infile.cpp is the code you want to compile." << endl;
+      cout << " " << endl;
+      cout << "Example: "<< argv[0] <<" outfile infile.cpp" << endl;
+      cout << "This will compile the infile.cpp to outfile." << endl;
+      cout << " " << endl;
+      cout << "Example: "<< argv[0] <<" infile.cpp" << endl;
+      cout << "This will compile the infile.cpp to infile." << endl;
+
+      return 0;
+    }
   }
 
-  cout << argv[0] << endl;
+  if ((argc < 2) || (argc > 3)) {             // Checks if user input wrong amount of variables
+    cerr << "Wrong input variables. Run with parameter --help for more." << endl;
+  }
+
+  if (argc == 2) {
+    simpleCompile(argv[1]);
+  } else if (argc == 3) {
+    compile(argv[1], argv[2]);
+  }
+
   return 0;
 }
---
+++ Code/C++/testcode.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "helooooo wourld" << std::endl;
+  return 0;
+}
