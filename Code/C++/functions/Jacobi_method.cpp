9db1ea669715b3b0c40abd55114b01e41cc44f80
amundmr <amundmr@github.com>
2019-09-22 18:40:34 +0200

    Tried adding comments to codes. Please check jacobi_meth and jacobi_rot

--- Code/C++/functions/Jacobi_method.cpp
+++ Code/C++/functions/Jacobi_method.cpp
@@ -1,19 +1,19 @@
-int off(mat A, int n, double max_off);
+int off(mat A, int n, double max_off);                      //Declaring off function
 
-void Jacobi_rotate(mat A, mat B, int k, int l, int n);
+void Jacobi_rotate(mat A, mat B, int k, int l, int n);      //Declaring Jacobi_rotate function
 
 mat Jacobi_method(mat A, int n){
-    double epsilon = 1.0e-8;
-    int max_iterations = 1e5;
+    double epsilon = 1.0e-8;                                //Defining maximum deviation from 0 for the off diagonal elements
+    int max_iterations = 1e5;                               //Setting a number of max iterations if the tolerance epsilon is not yet met
     int i = 0;
-    double max_off = 0.0;
+    double max_off = 0.0;                                   //Introducing variable for storing max off-diagonal value
     mat B;
-    
-    while (max_off > epsilon && i <= max_iterations){
+
+    while (max_off > epsilon && i <= max_iterations){       //Run this loop until max deviation is less than eps or max iterations is met
         B = A;
-        int k,l = off(A, n, max_off);
-        Jacobi_rotate(A,B,k,l,n);
-        A=B;
+        int k,l = off(A, n, max_off);                       //Gathers k,l(row & column of biggest off diagonal element)
+        Jacobi_rotate(A,B,k,l,n);                           //Rotates matrix A around max element A(k,l), and returns it as B
+        A=B;                                                //Setting A=B ready for new loop iteration
         i++;
     }
 }
--- Code/C++/functions/Jacobi_rotate.cpp
+++ Code/C++/functions/Jacobi_rotate.cpp
@@ -11,7 +11,7 @@ void Jacobi_rotate(mat A, mat B, int k, int l, int n){
     double sin;
     double cos;
 
-    if (A(k,l) != 0.0){
+    if (A(k,l) != 0.0){                                 //If A(k,l) is 0, don't rotate
         double tan;
         double tau;
         double t;
@@ -25,16 +25,19 @@ void Jacobi_rotate(mat A, mat B, int k, int l, int n){
 
         cos = 1/(sqrt(1.0+pow(tan,2)));
         sin = cos*tan;
-    }    
+    }
     else{
         cos = 1.0;
         sin = 0.0;
     }
+
+    //Rotating element (k,k) and (l,l) of matrix A in to matrix B
     B(k,k) = pow(cos,2)*A(k,k) - 2.0*cos*sin*A(k,l) + pow(sin,2)*A(l,l);
     B(l,l) = pow(sin,2)*A(k,k) + 2.0*cos*sin*A(k,l) + pow(cos,2)*A(l,l);
     B(k,l) = 0.0;
     B(l,k) = 0.0;
 
+    //Rotating all off-diagonal elements from A into B
     for (int i=0; i<n; i++){
         if (i != k && i != 1){
             B(i,k) = cos*A(i,k) - sin*A(i,l);
