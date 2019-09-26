560043fcf339e45a5b3f06040513b7a798d62b9c
annasro <anna.stray.rongve@gmail.com>
2019-09-26 10:23:54 +0200

    Max test + eigeinvalue test

--- Code/C++/testing_functions.cpp
+++ Code/C++/testing_functions.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 
 TEST_CASE("Test max a(i,j"){
-  int n = 3;
-  double pmin = 0, pmax = 10, h = (pmax-pmin)/(double(n));
+  int N = 3;
+  double pmin = 0, pmax = 10, h = (pmax-pmin)/(double(N));
   mat a = zeros<mat>(n,n);
   mat v = zeros<mat>(n,n);
   vec r(n);
@@ -20,5 +20,25 @@ TEST_CASE("Test max a(i,j"){
   REQUIRE(p==2);
   REQUIRE(q==1);
   REQUIRE(apq == Approx(-0.09));
+
+}
+
+
+TEST_CASE("Eigenvalues"){
+  int N = 5, interact = 0;
+  double conv = 0.001, wr =0.01, pmin = 0, pmax = 10, h = (pmax-min)/(double(N));
+  N = N-1;
+  mat a = zeros<mat>(n,n);
+  mat v = zeros<mat>(n,n);
+  vec r(n);
+
+  initialize(variables);
+  jacobi(variables)
+  
+  vector<double>eigenvalue=get_eigenvals(a,n);
+
+  REQUIRE(eigenvalue[0] = approx())
+  REQUIRE(eigenvalue[1] = approx())
+  REQIORE(eigenvalye[2] = approx())
 
 }
