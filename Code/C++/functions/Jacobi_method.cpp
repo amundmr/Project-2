fa68dd23b27d907a146e4eadbef478fa8526a641
Knut Magnus Aasrud <kmaasrud@outlook.com>
2019-09-26 08:35:15 +0200

    Merge of "b)" and "origin/b)"

--- Code/C++/functions/Jacobi_method.cpp
+++ Code/C++/functions/Jacobi_method.cpp
@@ -6,20 +6,20 @@ using namespace std;
 using namespace arma;
 
 mat Jacobi_rotate(mat A, int k, int l, int N);
-void off(mat A, int N, double* max, int* k, int* l);
+void off(mat A, int N, double * max, int * k, int * l);
 
 mat Jacobi_method(mat A, int N){
     double epsilon = 1.0e-8;                          //Defining maximum deviation from 0 for the off diagonal elements
     int max_iterations = 10;                         //Setting a number of max iterations if the tolerance epsilon is not yet met
-    int i = 0;
     double max = epsilon * 2;                         //Introducing variable for storing max off-diagonal value
-    //mat B = mat(N,N, fill::zeros);
     int k = 0; int l = 0;
-    while (max > epsilon && i < max_iterations){     //Run this loop until max deviation is less than eps or max iterations is met
 
+    int i = 0;
+    while (max > epsilon && i < max_iterations){     //Run this loop until max deviation is less than eps or max iterations is met
+        max = 0.0;
         off(A, N, &max, &k, &l);                      //Finding the maximum value and returning its indices k, l
         cout << "Rotation "<< i << ": " << endl;
-        cout << "Max indices (kl): " << k << l << endl;
+        cout << "max: " << max << endl;
 
         A = Jacobi_rotate(A,k,l,N);                     //Rotating by means of k, l.
 
--- Code/C++/functions/off.cpp
+++ Code/C++/functions/off.cpp
@@ -5,17 +5,15 @@
 using namespace std;
 using namespace arma;
 
-void off(mat A, int N, double * max, int * k, int * l){    //double* means it will be recieving a double POINTER. to get this value use *max
+void off(mat A, int N, double * max_pointer, int * k_pointer, int * l_pointer){    //double* means it will be recieving a double POINTER. to get this value use *max
    // finding the maximum of the off-diagonal elements.
-   for ( int i = 0 ; i < N ; ++i ){
-       for ( int j = i+1 ; j < N ; ++j ){
+   for ( int i = 0 ; i < N-1 ; ++i ){
+       for ( int j = i+1 ; j < N-1 ; ++j ){
 
-          double aij = fabs( A(i,j) );          //Looking at the floating absolute of the element in the matrix
+         double aij = fabs( A(i,j) );          //Looking at the floating absolute of the element in the matrix
 
-          if ( aij > *max ){                    //If the element is greater than any previous element
-            *max = aij;  *k = i; *l = j;        //Then we want to store its index and its value
-            cout << "max: " << *max << "kl: " << *k << *l << endl;
-
+         if ( aij > *max_pointer ){                    //If the element is greater than any previous element
+            *max_pointer = aij;  *k_pointer = i; *l_pointer = j;        //Then we want to store its index and its value
           }
        }
    }
