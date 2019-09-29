5e88b130696d458ad37dc4c018da9ed1e25cc816
Knut Magnus Aasrud <kmaasrud@outlook.com>
2019-09-29 12:00:22 +0200

    Bisection should work now, but unsure about the function f(x)
    I'm not completely sure how f is the actual characteristic polynomial. The return values seem a bit arbitrary...

--- Code/C++/bisection.cpp
+++ Code/C++/bisection.cpp
@@ -1,76 +1,55 @@
 #include <iostream>
 #include <math.h>   //fabs()
 #include <armadillo>
-#include <iomanip>
 
 using namespace std;
 using namespace arma;
 
-double f(double x); //declearing function
-double f(double x)  //defining function
-{
+double f(double x){
   double pi, diag = 2.0, nondiag = -1.0;
   int n = 10;
   vec p = zeros<vec>(n);
   p[1] = 1;
   p[0] = 0;
-  for (int i = 2; i <= n ; ++i)
-  {
-  pi = (diag - x) * (p[i-1]) - (nondiag * nondiag * (p[i-2]));
-  p[i] = pi;
+  for (int i = 2; i <= n ; ++i){
+    pi = (diag - x) * (p[i-1]) - (nondiag * nondiag * (p[i-2]));
+    p[i] = pi;
   }
-
+  return pi;
 }
 
-double bisection(double a, double b, int n)
-{
-  double c, epsilon = 1E-9, root, fa, fb, fc;
-  int i;
-    if ((f(a) * f(b)) > 0)         //root not in intervall
-    {
-      cout << "No roots in intervall [" <<a<< " , "<< b << "]"<<endl;;
-    }
-
-    //cout<<setw(12)<<"a"<<setw(18)<<"b"<<setw(18)<<"f(c)"<<setw(18)<<"|a-b|"<<endl;
-
-    while (fabs(b - a) >= epsilon)
-    {
-
+void bisection(double a, double b){
+  double c, epsilon = 1e-8, root, fa, fb, fc;
+  if ((f(a) * f(b)) > 0){ // Root not in interval, don't do anything
+  }
+  else{
+    while (fabs(b - a) >= epsilon){
       c = (a + b) /2.0;
-      fa = f(a);
-      fb = f(b);
-      fc = f(c);
-
-
-      //cout<<setw(18)<<a<<setw(18)<<b<<setw(18)<<fc<<setw(18)<<fabs(a-b)<<endl;
-
-        if (fc == 0.0)
-          {
-            cout <<"Root is: " << c << endl;
-            return 0;
-          }
-        if (fa*fc > 0)        //no root between a and c
-        {
-          a = c;
-        }
-        else              //Root lies here.
-        {
-          b = c;
-        }
-
+      
+      // If there's no root between a and c, set a=c
+      if (f(a)*f(c) > 0){
+        a = c;
       }
-  cout << "The root for f(c) " << f(c) << " is " << c << endl;
-    }
-
-    int main()
-    {
-      double a = -10.0, b = -9.0;
-        int n = 10;
-      for (int i = 0; i  <= 20; i++)
-      {
-        a += 1.0;
-        b += 1.0;
-        bisection(a,b,n);
+      // If there's a root between a and c, set a=b
+      else{
+        b = c;
       }
-
     }
+    cout <<"Root found: " << c << endl;
+  }
+}
+
+int main(){
+  double start = -10;
+  double end = 10;
+  int N = 1000;
+  double h = (end-start)/N;
+
+  cout << "Searching for roots in interval: [" << start << "," << end << "]" << endl << endl;
+  // Test for roots in small intervals from start to end
+  for (int i = 0; i <= 200; i++){
+    double a = h*i;
+    double b = h*(i+1);
+    bisection(a,b);
+  }
+}
