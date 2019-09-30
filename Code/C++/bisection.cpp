9042c5dc5cbe99498389654f5b93a659ca75f012
annasro <anna.stray.rongve@gmail.com>
2019-09-30 10:05:01 +0200

    bisection

--- Code/C++/bisection.cpp
+++ Code/C++/bisection.cpp
@@ -5,53 +5,45 @@
 using namespace std;
 using namespace arma;
 
-vec f(double x); //declearing function
-vec f(double x)  //defining function
+double f(double x); //declearing function
+double f(double x)  //defining function
 {
   double pi, diag = 2.0, nondiag = -1.0;
   double n = 10;
   vec p = zeros<vec>(n);
+
   p[1] = 1;
   p[0] = 0;
-<<<<<<< HEAD
+
   for (int i = 2; i <= n ; ++i)
   {
-  p[i] = (diag - x) * (p[i-1]) - (nondiag * nondiag * (p[i-2]));
+  pi = (diag - x)*(p[i-1]) - (nondiag*nondiag*(p[i-2]));
+  cout << "p[i]: "<< p[i]<<endl;
   }
-  return p;
+
+  return pi;
 }
 
 double bisection(double a, double b)
 {
-  double c, epsilon = 1E-9, root, fa_i, fb_i, fc_i;
+  double c, epsilon = 1E-9, root;
   int i;
-  vec fa, fb, fc;
+  double fa, fb, fc;
 
-  c = (a + b) /2.0;
-  fc[0] = f(c);
-  fb = f(b);
-  fa = f(a);
+    //cout<<setw(12)<<"a"<<setw(18)<<"b"<<setw(18)<<"f(c)"<<setw(18)<<"|a-b|"<<endl;
 
-  for (int i = 2; i <= 4; i++)
-  {
-
-    fa_i = fa[i];
-    fb_i = fb[i];
-    //fc_i = fc[i];
-
-    cout <<"fa: [" << fa <<" ]"<<endl;
-    cout <<"fai: " << fa_i <<endl;
-    cout <<"fb: " << fb <<endl;
-    cout <<"fbi: " << fb_i <<endl;
-
-    cout<<setw(12)<<"a"<<setw(18)<<"b"<<setw(18)<<"f(c)"<<setw(18)<<"|a-b|"<<endl;
-
-    if ((fa_i * fb_i) > 0)         //root not in intervall
+    if ((fa * fb) > 0)         //root not in intervall
     {
       cout << "No roots in intervall [" <<a<< " , "<< b << "]"<<endl;
     }
     while (fabs(b - a) >= epsilon)
     {
+      c = (a + b) /2.0;
+      fc = f(c);
+      fb = f(b);
+      fa = f(a);
+      cout <<"fa: " << fa <<endl;
+      cout <<"fb: " << fb <<endl;
       //cout<<setw(18)<<a<<setw(18)<<b<<setw(18)<<fc_i<<setw(18)<<fabs(a-b)<<endl;
 
       if (fc == 0.0)
@@ -60,7 +52,7 @@ double bisection(double a, double b)
         return 0;
       }
 
-      if (fa_i*fc > 0)        //no root between a and c
+      if (fa*fc > 0)        //no root between a and c
       {
         a = c;
       }
@@ -70,12 +62,7 @@ double bisection(double a, double b)
       }
     }
     cout << "The root for f(c) " << f(c) << " is " << c << endl;
-  }
 }
-
-
-
-
 
 
 int main()
@@ -84,33 +71,3 @@ int main()
   int n = 100;
   bisection(a,b);
 }
-=======
-
-void bisection(double a, double b){
-  double c, epsilon = 1e-8, root, fa, fb, fc;
-  if ((f(a) * f(b)) > 0){ // Root not in interval, don't do anything
-  }
-  else{
-    while (fabs(b - a) >= epsilon){
-      c = (a + b) /2.0;
-
-      // If there's no root between a and c, set a=c
-      if (f(a)*f(c) > 0){
-        a = c;
-      }
-      // If there's a root between a and c, set a=b
-      else{
-        b = c;
-      }
-    }
-    cout <<"Root found: " << c << endl;
-  }
-}
-
-int main(){
-  int N = 1000;
-    double a = 0.0, b = 1.0;
-    bisection(a,b);
-
-}
->>>>>>> 5e88b130696d458ad37dc4c018da9ed1e25cc816
