348af83ffbd95dd49cb4ff78c640188ed36efa0e
annasro <anna.stray.rongve@gmail.com>
2019-10-01 10:10:14 +0200

    Revert "btiming bisection works, missing jacobi in here"
    This reverts commit 05de3714b40661b482c5078d770fa5b14865e883.

--- Code/Bisection/bisection.cpp
+++ Code/Bisection/bisection.cpp
@@ -1,4 +1,3 @@
-//compile as g++ bisection.cpp -larmadillo -lblas -llapack
 #include <iostream>
 #include <math.h>   //fabs()
 #include <armadillo>
@@ -6,12 +5,9 @@
 using namespace std;
 using namespace arma;
 
- //declaring functions
 vec Jacobi_method(mat A, int N);
-double f(double x, int n);
-void bisection(double a, double b, int n);
 
-double f(double x, int n)  //defining function for characteristic polynomial
+double f(double x, int n)  //defining function
 {
   double f, diag = 2.0, nondiag = -1.0;
   vec p = zeros<vec>(n);
@@ -26,9 +22,9 @@ double f(double x, int n)  //defining function for characteristic polynomial
 
   return f;
 }
-//Bisection method
+
 void bisection(double a, double b, int n){
-  double c, epsilon = 1e-15, root, fa, fb, fc;
+  double c, epsilon = 1e-8, root, fa, fb, fc;
   if ((f(a,n) * f(b,n)) > 0){ // Root not in interval, don't do anything
   }
   else{
@@ -49,23 +45,10 @@ void bisection(double a, double b, int n){
 }
 
 int main(){
-
   int n = 20;
   int N = 10000;
-  double start = 0.0, end = 1.00;
+  double start = -10.0, end = 10.0;
   double h = (end-start)/N;
-
-  mat A(n,n, fill::zeros);
-  A.diag() += 2;
-  A.diag(-1) -= 1;
-  A.diag(1) -= 1;
-
-  vec Jacobi_method(mat A, int N);
-  double f(double x, int n);
-  void bisection(double a, double b, int n);
-
-
-
   //timing for comparason
 
   clock_t t0,t1;
@@ -76,22 +59,18 @@ int main(){
     bisection(a,b,n);
   }
     t1 = clock();
-    double t_bisec = (double (t1 - t0))/CLOCKS_PER_SEC; //tme spent on biesection solver.
+    double t_bisec = (double (t1 - t0))/CLOCKS_PER_SEC;
 
+  mat A(n,n, fill::zeros);
+  A.diag() += 2;
+  A.diag(-1) -= 1;
+  A.diag(1) -= 1;
 
-
-/*timing to compare bisection with jacobi
+//timing to compare bisection with jacobi
   t0 = clock();
   vec eigvals = Jacobi_method(A, n);
   t1 = clock();
   double t_jacobi = (double (t1- t0)) /CLOCKS_PER_SEC;	// Time spent on our eigenvalue solver.
 
 
-	cout << "------------------------------------" << endl;
-	cout << "Time spent on our solver: " << t_jacobi << "s" << endl;
-
-*/
-
-  cout << "------------------------------------" << endl;
-	cout << "Time spent on Bisection method: " << t_bisec << "s" << endl;
 }
